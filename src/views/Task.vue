99c27dfff8de19a09722a61450bd4bea918c4e1f
Paul Ezaga <paulezaga@gmail.com>
2020-08-04 14:48:35 +0200

    TodoApp first Phase Implementation

---
+++ src/views/Task.vue
@@ -0,0 +1,26 @@
+<template>
+    <li>
+        <div>
+           {{ task.name }} 
+           {{index}} 
+            <small> {{ task.dueDate }} @ {{ task.dueTime }} </small>
+            <button @click="deleteTask(index)">X</button>
+        </div>
+    </li>    
+</template>
+
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Task extends Vue {
+    @Prop() task!: object;
+    @Prop() index!: number;
+    @Prop() deleteTask!: Function;
+
+    private mounted(){
+        console.log(this.task);
+    }
+}
+</script>
